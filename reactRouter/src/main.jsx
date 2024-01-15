f7a0da5bf8edc0860ebdb62507a1226f098be8ac
addy-301 <vaditya832@gmail.com>
2024-01-15 20:49:26 +0530

    router fixed

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -6,7 +6,8 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 
-const router=createBrowserRouter({
+const router=createBrowserRouter([
+  {
     path: '/',
     element: <Layout />,
     children: [
@@ -19,7 +20,8 @@ const router=createBrowserRouter({
         element: <About />
       }
     ]
-})
+  }
+  ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
