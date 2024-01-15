0d2f1a9204e5502624082ae7d1e450749abff115
addy-301 <vaditya832@gmail.com>
2024-01-15 23:08:18 +0530

    optimized

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,16 +1,16 @@
-//import { useLoaderData } from 'react-router-dom'
-import {useState, useEffect} from 'react'
+import { useLoaderData } from 'react-router-dom'
+
 function Github() {
-    //const data = useLoaderData()
-    const [data, setData] = useState([])
-    useEffect(() => {
-     fetch('https://api.github.com/users/addy-301')
-     .then(response => response.json())
-     .then(data => {
-        console.log(data);
-        setData(data)
-     })
-    }, [])
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
     
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
@@ -20,8 +20,8 @@ function Github() {
 }
 
 export default Github
-/*
+
 export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/addy-301')
     return response.json()
-}*/
+}
