4d04a53e9d35df0550a9bb7e1eab972a6418803d
addy-301 <vaditya832@gmail.com>
2024-09-04 21:14:52 +0530

    final commit

--- todoRTK/src/App.jsx
+++ todoRTK/src/App.jsx
@@ -1,7 +1,12 @@
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import './App.css'
 function App() {
   return (
    <>
     <h1>RTK</h1>
+    <AddTodo />
+    <Todos />
    </>
   )
 }
